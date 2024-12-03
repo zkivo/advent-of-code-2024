5077531eff7a527f1641c792059e4d166e32bd2a
Marco Schivo <marcoschivo1@gmail.com>
2024-12-03 23:40:57 +0100

    added part2 day3

--- day3/code.sh
+++ day3/code.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
-#
-#
-#           IT DOES NOT WORK!!
-#
-#   I think is because there is no float in bash, therefore cannot store
-#      big numbers. :(
-#
-#
+# Part 1
+cat input.txt | grep -oP "mul\\(\d+,\d+\\)" | grep -oP '\d+,\d+' | awk -F, '{ sum += $1 * $2 } END { print sum }'
 
-# Define your regex
-regex="mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"
-sum=0
-# Read from standard input
-while read -r line; do
-    # Use a while loop with grep -o to find all matches of the regex in the line
-    while [[ $line =~ $regex ]]; do
-        match="${BASH_REMATCH[0]}" # Extract the matched text
-        echo $match ${BASH_REMATCH[1]} ${BASH_REMATCH[2]}
-        (( sum += ${BASH_REMATCH[1]} * ${BASH_REMATCH[2]} )) # Add the product to the sum
-
-        # Modify the line to avoid infinite loop (remove the first match)
-        line="${line/${BASH_REMATCH[0]}/}"
-    done
-done
-
-echo $sum
+# Part 2
+cat input.txt | grep -oP "mul\\((\d+),(\d+)\\)|do\(\)|don't\(\)" | sed 's/mul//g' | sed 's/(\|)//g' | sed 's/,/ /g' | awk -v flag=1 -v sum=0 '{ if ($1 == "do") { flag=1 } else if ($1 == "don'\''t") { flag=0 } else { if (flag == 1) {sum += $1 * $2} } } END { print sum }'
