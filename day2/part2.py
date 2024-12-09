1825aad99e2f4094228ac763234b82e3474627cf
Marco Schivo <marcoschivo1@gmail.com>
2024-12-09 23:07:51 +0100

    day9

--- day2/part2.py
+++ day2/part2.py
@@ -3,50 +3,40 @@ import numpy as np
 # File path to the input file
 file_path = 'input.txt'
 
-with open(file_path, 'r') as file:
-    matrix = np.loadtxt(file, dtype=int)
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+
+def get_index(row):
+    asc = None
+    for i, number in enumerate(row):
+        if i == len(row) - 1: continue
+        left, right = row[i], row[i + 1]
+        if i == 0: # ascending or descending
+            if right - left > 0:
+                asc = 1
+            else:
+                asc = -1
+        diff = right - left
+        if diff * asc < 0:
+            return i
+        diff = abs(diff)
+        if diff < 1 or diff > 3:
+            return i
+    return None
 
 sum = 0
-for i, row in enumerate(matrix):
-    print(row)
-    asc = None
-    redo = False
-    valid = True
-    while valid:
-        for j, number in enumerate(row):
-            if j == len(row) - 1: continue
-            left, right = row[j], row[j + 1]
-            if j == 0: # ascending or descending
-                if right - left > 0:
-                    asc = True
-                else:
-                    asc = False
-            diff = right - left
-            if diff * asc < 0:
-                print(f"{row[j]}, {row[j+1]}: not monotone")
-                # remove the right element
-                row = np.delete(row, j + 1)
-                if redo == True:
-                    redo = False
-                    valid = False
-                    break
-                redo = True
-                break
-            else:
-                diff = abs(diff)
-                if diff < 1 or diff > 3:
-                    print(f"diff({row[j]}, {row[j+1]})= {diff}, not in range")
-                    row = np.delete(row, j + 1)
-                    if redo == True:
-                        redo = False
-                        valid = False
-                        break
-                    redo = True
-                    break
-        if redo:
-            continue
-        if valid:
+for row in lines:
+    row = np.array([int(x) for x in row.split()])
+    ret = get_index(row)
+    if ret is None:
+        sum += 1
+        continue
+    for i in range(len(row)):
+        new_row = np.delete(row, i)
+        if get_index(new_row) is None:
             sum += 1
+            print(new_row, 'safe')
             break
+    print(row, 'unsafe')
 print(sum)
 
